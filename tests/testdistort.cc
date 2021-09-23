18f05c2252894cacd8b1b1f5216f81200b60d17f
Stefan Westerfeld <stefan@space.twc.de>
2021-09-23 16:38:09 +0200

    Support zita resampler as expected output for distortion test.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/testdistort.cc
+++ tests/testdistort.cc
@@ -1,9 +1,36 @@
 #include "pandaresampler.hh"
 #include <math.h>
 #include <assert.h>
+#include <cstring>
+#include <zita-resampler/resampler.h>
 
 using PandaResampler::Resampler2;
 using std::vector;
+
+void
+zita_resample (vector<float>& in, int from, int to, vector<float>& out)
+{
+  Resampler resampler;
+  const int hlen = 32;
+  assert (resampler.setup (from, to, 1, hlen) == 0);
+
+  resampler.out_count = out.size() / resampler.nchan();
+  resampler.out_data = &out[0];
+
+  /* avoid timeshift: zita needs k/2 - 1 samples before the actual input */
+  resampler.inp_count = resampler.inpsize () / 2 - 1;
+  resampler.inp_data  = nullptr;
+  resampler.process();
+
+  resampler.inp_count = in.size() / resampler.nchan();
+  resampler.inp_data = (float *) &in[0];
+  resampler.process();
+
+  /* zita needs k/2 samples after the actual input */
+  resampler.inp_count = resampler.inpsize() / 2;
+  resampler.inp_data  = nullptr;
+  resampler.process();
+}
 
 int
 main (int argc, char **argv)
@@ -21,6 +48,9 @@ main (int argc, char **argv)
       in[i] = sin (phase);
     }
   int over = atoi (argv[1]), stages;
+  bool zita = strcmp (argv[2], "zita") == 0;
+  bool panda = strcmp (argv[2], "panda") == 0;
+  assert (zita || panda);
   if (over == 0)
     {
       for (size_t i = 0; i < in.size(); i++)
@@ -41,26 +71,51 @@ main (int argc, char **argv)
               break;
       default: assert (false);
     }
-  for (int s = 0; s < stages; s++)
+  if (panda)
     {
-      Resampler2 ups (Resampler2::UP, Resampler2::PREC_96DB);
-      vector<float> out (in.size() * 2);
-      ups.process_block (&in[0], in.size(), &out[0]);
+      for (int s = 0; s < stages; s++)
+        {
+          Resampler2 ups (Resampler2::UP, Resampler2::PREC_96DB);
+          vector<float> out (in.size() * 2);
+          ups.process_block (&in[0], in.size(), &out[0]);
+          in = out;
+        }
+    }
+  else
+    {
+      vector<float> out;
+      out.resize (in.size() * over);
+      zita_resample (in, 48000, 48000 * over, out);
       in = out;
     }
   for (size_t i = 0; i < in.size(); i++)
     {
       in[i] = atan (in[i] * 3) / M_PI * 2;
     }
-  for (int s = 0; s < stages; s++)
+  if (panda)
     {
-      Resampler2 downs (Resampler2::DOWN, Resampler2::PREC_96DB);
-      vector<float> out (in.size() / 2);
-      downs.process_block (&in[0], in.size(), &out[0]);
+      for (int s = 0; s < stages; s++)
+        {
+          Resampler2 downs (Resampler2::DOWN, Resampler2::PREC_96DB);
+          vector<float> out (in.size() / 2);
+          downs.process_block (&in[0], in.size(), &out[0]);
+          in = out;
+        }
+    }
+  else
+    {
+      vector<float> out;
+      out.resize (in.size() / over);
+      zita_resample (in, 48000 * over, 48000, out);
       in = out;
+    }
+  float in_max = 0;
+  for (size_t i = 0; i < in.size(); i++)
+    {
+      in_max = std::max (fabs (in[i]), in_max);
     }
   for (size_t i = 0; i < in.size(); i++)
     {
-      printf ("%f\n", in[i]);
+      printf ("%f\n", in[i] / in_max);
     }
 }
