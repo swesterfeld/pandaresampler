994737b74c891f6bc79cc3ae50fbc9f8d677acd3
Stefan Westerfeld <stefan@space.twc.de>
2021-09-29 16:59:01 +0200

    TESTS: add test for multi-stage downsampling filter
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ tests/testdownmulti.cc
@@ -0,0 +1,89 @@
+#include "pandaresampler.hh"
+#include <math.h>
+#include <fftw3.h>
+
+using PandaResampler::Resampler2;
+using std::vector;
+
+inline double
+window_blackman (double x)
+{
+  if (fabs (x) > 1)
+    return 0;
+  return 0.42 + 0.5 * cos (M_PI * x) + 0.08 * cos (2.0 * M_PI * x);
+}
+
+void
+fft (const uint n_values, float *r_values_in, float *ri_values_out)
+{
+  auto plan_fft = fftwf_plan_dft_r2c_1d (n_values, r_values_in, (fftwf_complex *) ri_values_out, FFTW_ESTIMATE | FFTW_PRESERVE_INPUT);
+
+  fftwf_execute_dft_r2c (plan_fft, r_values_in, (fftwf_complex *) ri_values_out);
+
+  // usually we should keep the plan, but for this simple test program, the fft
+  // is only computed once, so we can destroy the plan here
+  fftwf_destroy_plan (plan_fft);
+}
+
+double
+db (double x)
+{
+  return 20 * log10 (std::max (x, 0.0000001));
+}
+
+void
+check_amps (int ds, double freq)
+{
+  const int FFT_SIZE = 8192;
+  vector<float> in (FFT_SIZE * ds);
+  vector<float> out (FFT_SIZE);
+
+  double normalize = 0;
+  for (size_t i = 0; i < in.size(); i++)
+    {
+      double w = window_blackman ((2.0 * i - FFT_SIZE) / FFT_SIZE);
+      normalize += w;
+      in[i] = sin (i * freq / (44100 * ds) * 2 * M_PI) * w;
+    }
+  normalize /= 2 * ds;
+
+  Resampler2 downs (Resampler2::DOWN, ds, Resampler2::PREC_96DB);
+  downs.process_block (&in[0], in.size(), &out[0]);
+
+  vector<float> fft_out (FFT_SIZE + 2 /* fftw extra complex output value */);
+  fft (FFT_SIZE, &out[0], &fft_out[0]);
+
+  /*
+  for (size_t i = 0; i < in.size(); i++)
+    printf ("%zd %.17g #IN\n", i, in[i]);
+
+  for (size_t i = 0; i < out.size(); i++)
+    printf ("%zd %.17g #OUT\n", i, out[i]);
+  */
+
+  float passband_amp = 0;
+  float transband_amp = 0;
+  for (size_t i = 0; i < fft_out.size(); i += 2)
+    {
+      float freq = (i / 2) / double (FFT_SIZE) * 44100;
+      float amp = sqrt (fft_out[i] * fft_out[i] + fft_out[i+1] * fft_out[i+1]) / normalize;
+
+      if (freq < 18000)
+        passband_amp = std::max (passband_amp, amp);
+      else
+        transband_amp = std::max (transband_amp, amp);
+      //printf ("%f %.17g #FFT\n", freq, amp);
+    }
+  printf ("%f %.17g %.17g # PASS/TRANS\n", freq, db (passband_amp), db (transband_amp));
+}
+
+int
+main (int argc, char **argv)
+{
+  const int ds = atoi (argv[1]);
+
+  for (float freq = 100; freq < 22050 * ds; freq += 100)
+    check_amps (ds, freq);
+
+  return 0;
+}
