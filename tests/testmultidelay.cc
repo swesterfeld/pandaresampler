1f238747349669fa457a3a34995556ee40a04e89
Stefan Westerfeld <stefan@space.twc.de>
2021-10-27 17:56:27 +0200

    TESTS: support oversampling in multi-delay test
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/testmultidelay.cc
+++ tests/testmultidelay.cc
@@ -9,9 +9,10 @@ using PandaResampler::Resampler2;
 int
 main (int argc, char **argv)
 {
-  bool up = strcmp (argv[1], "up") == 0;
-  bool down = strcmp (argv[1], "down") == 0;
-  assert (up || down);
+  const bool up = strcmp (argv[1], "up") == 0;
+  const bool down = strcmp (argv[1], "down") == 0;
+  const bool over = strcmp (argv[1], "over") == 0;
+  assert (up || down || over);
 
   const int ratio = atoi (argv[2]);
 
@@ -24,32 +25,60 @@ main (int argc, char **argv)
 
   assert (fir || iir || iir_sse);
 
-  Resampler2 res (up ? Resampler2::UP : Resampler2::DOWN, ratio, prec, sse, fir ? Resampler2::FILTER_FIR : Resampler2::FILTER_IIR);
+  Resampler2 ups (Resampler2::UP, ratio, prec, sse, fir ? Resampler2::FILTER_FIR : Resampler2::FILTER_IIR);
+  Resampler2 downs (Resampler2::DOWN, ratio, prec, sse, fir ? Resampler2::FILTER_FIR : Resampler2::FILTER_IIR);
 
   const double rate = 44100;
-  const double new_rate = rate * (up ? ratio : 1. / ratio);
-
+  const double freq = 1000;
   constexpr int SAMPLES = 1024;
 
   alignas(16) float in[SAMPLES] = { 0, };
   alignas(16) float out[SAMPLES * ratio] = { 0, };
 
   for (int i = 0; i < SAMPLES; i++)
-    in[i] = sin (i * 1000. / rate * 2 * M_PI);
-  res.process_block (in, SAMPLES, out);
+    in[i] = sin (i * freq / rate * 2 * M_PI);
 
-  double delay = res.delay();
+  double new_rate;
+  int out_samples;
+  double delay;
+
+  if (up)
+    {
+      out_samples = SAMPLES * ratio;
+      new_rate = rate * ratio;
+
+      ups.process_block (in, SAMPLES, out);
+      delay = ups.delay();
+    }
+  else if (down)
+    {
+      out_samples = SAMPLES / ratio;
+      new_rate = rate / ratio;
+
+      downs.process_block (in, SAMPLES, out);
+      delay = downs.delay();
+    }
+  else // over
+    {
+      out_samples = SAMPLES;
+      delay = ups.delay() / ratio + downs.delay();
+      new_rate = rate;
+
+      alignas(16) float tmp[SAMPLES * ratio] = { 0, };
+      ups.process_block (in, SAMPLES, tmp);
+      downs.process_block (tmp, SAMPLES * ratio, out);
+    }
 
   printf ("# new_rate = %f delay = %f\n", new_rate, delay);
 
-  for (int i = 0; i < (up ? (SAMPLES * ratio) : (SAMPLES / ratio)); i++)
+  for (int i = 0; i < out_samples; i++)
     {
       double pos = i - delay;
       double s = 0;
 
       if (pos >= 0)
-        s = sin (pos * 1000. / new_rate * 2 * M_PI);
+        s = sin (pos * freq / new_rate * 2 * M_PI);
 
-      printf ("%d %f %f\n", i, out[i], s);
+      printf ("%d %.10f %.10f\n", i, out[i], s);
     }
 }
