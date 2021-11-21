fa868b0cfe687ba7cc3fb90be133489451911af1
Stefan Westerfeld <stefan@space.twc.de>
2021-11-21 17:39:51 +0100

    Use MPL-2.0 for all code (except hiir).
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/testheaderonly2.cc
+++ tests/testheaderonly2.cc
@@ -1,3 +1,4 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 #define PANDA_RESAMPLER_HEADER_ONLY
 
 #include "pandaresampler.hh"
--- tests/testsimple.cc
+++ tests/testsimple.cc
@@ -1,3 +1,4 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 #include "pandaresampler.hh"
 #include <math.h>
 
