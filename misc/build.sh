f00fe408535c85b1dc785acd31a5ab0eb3db7070
Stefan Westerfeld <stefan@space.twc.de>
2024-06-08 12:27:21 +0200

    MISC: add helper scripts for CI
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ misc/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+build()
+{
+  if [ -f "./configure" ]; then
+    make uninstall
+    make distclean
+  fi
+  echo "###############################################################################"
+  echo "# BUILD TESTS :"
+  echo "#   CC=$CC CXX=$CXX "
+  echo "#   ./autogen.sh $@"
+  echo "###############################################################################"
+  $CXX --version | sed '/^[[:space:]]*$/d;s/^/#   /'
+  echo "###############################################################################"
+  ./autogen.sh "$@"
+  make -j `nproc` V=1
+  if ! make -j `nproc` check; then
+    for LOG in $(find tests -iname '*.log')
+    do
+      echo "===== $LOG ====="
+      cat $LOG
+    done
+    exit 1
+  fi
+  make install
+}
+
+# Tests using gcc
+export CC=gcc CXX=g++
+
+build --enable-asan --enable-debug-cxx
+
+build
+make -j `nproc` distcheck
+
+# Tests clang
+export CC=clang CXX=clang++
+
+build
