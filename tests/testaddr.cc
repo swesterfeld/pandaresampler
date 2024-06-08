6e47d7658fa9897d2c3418afa2139eaa318fbc3f
Stefan Westerfeld <stefan@space.twc.de>
2024-06-08 15:52:19 +0200

    TESTS: testaddr: test downsampler as well
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/testaddr.cc
+++ tests/testaddr.cc
@@ -7,17 +7,23 @@ int main (int argc, char **argv)
 {
   for (uint M = 0; M < 100; M++)
     {
-      for (auto o : {1, 2, 4, 8, 16 })
+      for (auto over : { 1, 2, 4, 8 })
         {
-          for (auto p : {1, 8, 12, 16, 20, 24})
+          for (auto p : { 1, 8, 12, 16, 20, 24 })
             {
-              Resampler2 ups (Resampler2::UP, o, Resampler2::find_precision_for_bits (p), true);
+              Resampler2 ups (Resampler2::UP, over, Resampler2::find_precision_for_bits (p), true);
+              Resampler2 downs (Resampler2::DOWN, over, Resampler2::find_precision_for_bits (p), true);
 
               /* do this to check we don't access areas of the stack after the end of the input
                * need to use --enable-asan to trigger it
                */
-              float in[M], out[M * o];
+              float in[M], out[M * over];
               ups.process_block (&in[0], M, &out[0]);
+
+              /* this doesn't help for post-end validation due to internal buffers, but do it anyway
+               * to maybe catch other problems
+               */
+              downs.process_block (&out[0], M * over, &in[0]);
             }
         }
     }
